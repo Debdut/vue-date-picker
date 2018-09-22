b78504e3f3a5f5960481f1a3687b6b58988262b7
diptajit basak <diptajitbabubasak@gmail.com>
2018-09-22 19:07:50 +0530

    picker added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template lang="pug">
   #app
-    date-picker(:options="{ type: 'monthPicker' }")
+    //- date-picker(:options="{ type: 'monthPicker' }")
+    //- customInput
+    Picker
 </template>
 
 <script>
 import DatePicker from './components/DatePicker'
+import CustomInput from './components/CustomInput'
+import Picker from './components/Picker'
 
 export default {
   name: 'app',
   components: {
-    DatePicker
+    DatePicker,
+    CustomInput,
+    Picker
   }
 }
 </script>
@@ -21,6 +27,6 @@ export default {
   -moz-osx-font-smoothing: grayscale
   color: #2c3e50
   margin-top: 60px
-  margin-left: 300px
+  // margin-left: 300px
   margin-right: 300px
 </style>
